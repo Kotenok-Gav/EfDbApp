f2847039145f0c746f9ecd8760e523ec690a2986
Kotenok-Gav <nefedov_ilya96@mail.ru>
2022-04-18 01:07:26 +0500

    Добавьте файлы проекта.

---
+++ EfDbApp/MobileContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+
+public class MobileContext : DbContext
+{
+    public MobileContext() : base("DefaultConnection")
+    {
+
+    }
+    public DbSet<Phone> Phones { get; set; }
+}
